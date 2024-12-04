a17af691ed6c5857eafffe1ed27844e4ddbfe9e5
akwaii <alana04.g@gmail.com>
2024-12-03 20:11:21 -0600

    .

--- EmployeeManagmentSystem/src/Validation.java
+++
@@ -1,7 +0,0 @@
-public class Validation {
-    public static boolean validateEvaluationData(String employeeID, String criteria, String comments) {
-        return employeeID != null && !employeeID.trim().isEmpty() &&
-               criteria != null && !criteria.trim().isEmpty() &&
-               comments != null && !comments.trim().isEmpty();
-    }
-}
